50f4e4470a4b16d128c17d59b9bb2ec1940767fa
Guilherme <jg21504956@gmail.com>
2026-06-10 16:50:22 -0300

    feat: adicionando a estrutura inicial do projeto

---
+++ api/CineTickets/src/test/java/io/github/CineTickets/CineTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.CineTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
